886af9c178f4b0571f33dabcdbd159d75639fe5b
Arianaauto <lscottbernard@gmail.com>
2026-02-24 11:31:16 +0200

    Create page.tsx

---
+++ src/styles/src/app/src/app/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>ARIANA AUTO SALES LLC</h1>
+      <p>Site is working.</p>
+    </div>
+  );
+}
